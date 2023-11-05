fd0e9ac2df790a20dfc64edb9cbb1039a1174adc
Berke Sayın <sayinberkesayin@gmail.com>
2023-11-05 16:39:58 +0300

    Use ActivityIndicator component

--- App.js
+++ App.js
@@ -1,23 +1,19 @@
-import React, {useState} from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import React from 'react';
+import {View, StyleSheet} from 'react-native';
+import {MyActivityIndicator} from './app/components/MyActivityIndicator';
 
 export const App = () => {
-  const [textInfo, setTextInfo] = useState('React Native Quickstart');
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>{textInfo}</Text>
+      <MyActivityIndicator />
     </View>
   );
 };
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
     backgroundColor: '#F5FCFF',
-  },
-  text: {
-    fontSize: 24,
-    color: '#333',
+    // justifyContent: 'center',
+    // alignItems: 'center',
   },
 });
---
+++ app/components/MyActivityIndicator.js
@@ -0,0 +1,26 @@
+// ActivityIndicator
+// It displays a circular loading indicator.
+
+import React from 'react';
+import {ActivityIndicator, StyleSheet, View} from 'react-native';
+
+export const MyActivityIndicator = () => (
+  <View style={[styles.container, styles.horizontal]}>
+    <ActivityIndicator />
+    <ActivityIndicator size="large" />
+    <ActivityIndicator size="small" color="#0000ff" />
+    <ActivityIndicator size="large" color="#00ff00" />
+  </View>
+);
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  horizontal: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    padding: 10,
+  },
+});
