cd4d3b68a7ea989d1d12833e13235b21999d609d
Berke Sayın <sayinberkesayin@gmail.com>
2023-11-05 15:34:16 +0300

    Use StyleSheet component and separate styles, instead of using inline styling

--- App.js
+++ App.js
@@ -1,16 +1,22 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet} from 'react-native';
 
 export const App = () => {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#F5FCFF',
-      }}>
-      <Text style={{fontSize: 24, color: '#333'}}>Hello World!</Text>
+    <View style={styles.container}>
+      <Text style={styles.text}>Hello World!</Text>
     </View>
   );
 };
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  text: {
+    fontSize: 24,
+    color: '#333',
+  },
+});
