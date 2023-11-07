12d6c6ddff821ce75dcba4589e50366d3338a071
Berke Sayın <sayinberkesayin@gmail.com>
2023-11-07 12:59:47 +0300

    Use Image component

--- App.js
+++ App.js
@@ -2,14 +2,16 @@ import React from 'react';
 import {View, StyleSheet} from 'react-native';
 // import {MyActivityIndicator} from './app/components/MyActivityIndicator';
 // import {MyButton} from './app/components/MyButton';
-import {MyFlatList} from './app/components/MyFlatList';
+// import {MyFlatList} from './app/components/MyFlatList';
+import {MyImage} from './app/components/MyImage';
 
 export const App = () => {
   return (
     <View style={styles.container}>
       {/* <MyActivityIndicator /> */}
       {/* <MyButton /> */}
-      <MyFlatList />
+      {/* <MyFlatList /> */}
+      <MyImage />
     </View>
   );
 };
---
+++ app/components/MyImage.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import {View, Image, StyleSheet} from 'react-native';
+
+export const MyImage = () => {
+  return (
+    <View style={styles.container}>
+      <Image
+        style={styles.tinyLogo}
+        source={{
+          uri: 'https://reactnative.dev/img/tiny_logo.png',
+        }}
+      />
+      <Image
+        style={styles.tinyLogo}
+        source={{
+          uri: 'https://reactnative.dev/img/tiny_logo.png',
+        }}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  tinyLogo: {
+    width: 50,
+    height: 50,
+  },
+});
